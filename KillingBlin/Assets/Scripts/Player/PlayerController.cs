027b7bc6803c0242ba4f1c847ea16ec57bf3fbf0
J1YEAH <81518256+sohnjiye@users.noreply.github.com>
2024-06-21 23:41:47 +0900

    [Feat] 카메라컨트롤러 추가 및 플레이어 애니메이션 추가
    플레이어 키코드 추가 및 수정

---
+++ KillingBlin/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 달린다
+        //if (Input.GetKeyDown())
+        //{
+        //    this.animator.SetTrigger("RunTrigger");
+        //}
+
+        // 점프한다.
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.animator.SetTrigger("RunTrigger");
+        }
+
+        // 활 공격
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.animator.SetTrigger("isBowAttacking");
+        }
+
+        // 마법 공격
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.animator.SetTrigger("isMagicAttacking");
+        }
+
+        // 그냥 공격
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.animator.SetTrigger("isNormalAttacking");
+        }
+
+        // 활 특수 공격
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.animator.SetTrigger("isBowCriticalAttacking");
+        }
+
+        // 마법 특수 공격
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.animator.SetTrigger("isMagicCriticalAttacking");
+        }
+
+        // 그냥 특수 공격
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.animator.SetTrigger("isNormalCriticalAttacking");
+        }
+
+        // 수정 필요
+        if (/*플레이어의 체력이 0이 되었을때*/)
+        {
+            this.animator.SetTrigger("Death");
+        }
+
+
+        // 좌우 이동
+        int key = 0;
+        if (Input.GetKey(KeyCode.D))
+            key = 1;
+        if (Input.GetKey(KeyCode.A))
+            key = -1;
+
+        // 플레이어의 속도
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        // 스피드 제한
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        // 움직이는 방향에 따라 반전한다.
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        // 플레이어 속도에 맞춰 애니메이션 속도를 바꾼다.
+        if (this.rigid2D.velocity.y == 0)
+        {
+            this.animator.speed = speedx / 2.0f;
+        }
+        else
+        {
+            this.animator.speed = 1.0f;
+        }
+
+
+        // 충돌 판정에 의해 플레이어의..
+        void OnTriggerEnter2D(Collider other)
+        {
+            Debug.Log("충돌");
+        }
+    }
+}
